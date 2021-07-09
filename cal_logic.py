c39aac092febbfbdad1781a29206ca087c2eeaad
ASERIA3 <82015989+ASERIA3@users.noreply.github.com>
2021-07-09 21:41:09 +0800

    Create cal_logic.py

---
+++ cal_logic.py
@@ -0,0 +1,79 @@
+from tkinter import StringVar, Entry, Tk
+
+
+expression = ""
+
+def press(num):
+	# point out the global expression variable
+	global expression
+
+	# concatenation of string
+	expression = expression + str(num)
+
+	# update the expression by using set method
+	equation.set(expression)
+
+
+# Function to evaluate the final expression
+def equalpress():
+	# Try and except statement is used
+	# for handling the errors like zero
+	# division error etc.
+
+	# Put that code inside the try block
+	# which may generate the error
+	try:
+
+		global expression
+
+		# eval function evaluate the expression
+		# and str function convert the result
+		# into string
+		total = str(eval(expression))
+
+		equation.set(total)
+
+		# initialize the expression variable
+		# by empty string
+		expression = ""
+
+	# if error is generate then handle
+	# by the except block
+	except:
+
+		equation.set(" error ")
+		expression = ""
+
+
+# Function to clear the contents
+# of text entry box
+def clear():
+	global expression
+	expression = ""
+	equation.set("")
+
+if __name__ == "__main__":
+	# create a GUI window
+	gui = Tk()
+
+	# set the background colour of GUI window
+	gui.configure(background="light blue")
+
+	# set the title of GUI window
+	gui.title("Calculator")
+
+	# set the configuration of GUI window
+	gui.geometry("280x150")
+
+	# StringVar() is the variable class
+	# we create an instance of this class
+	equation = StringVar()
+
+	# create the text entry box for
+	# showing the expression .
+	expression_field = Entry(gui, textvariable=equation)
+
+	# grid method is used for placing
+	# the widgets at respective positions
+	# in table like structure .
+	expression_field.grid(columnspan=4, ipadx=70)
